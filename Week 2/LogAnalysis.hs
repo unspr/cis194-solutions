671cc9681834d93920a5464bbfea146e4e98ead1
todd <unspr@qq.com>
2020-09-09 00:08:58 +0800

    complete parseMessage.

--- Week 2/LogAnalysis.hs
+++ Week 2/LogAnalysis.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 module LogAnalysis where
 import Log
-import Debug.Trace
+-- import Debug.Trace
 
 --此处有编译原理中产生式的思想
 data IntStr = IntStr Int IntStr | Str String
@@ -22,19 +22,22 @@ getNum c = case c of
     _ -> 10
 
 parseIntStr :: IntStr -> IntStr
-parseIntStr (IntStr n (Str (x:str))) = case getNum x of
-    10 -> IntStr n (Str str)
-    _ -> parseIntStr (IntStr (n*10 + (getNum x)) (Str str))
+parseIntStr (IntStr n (Str (x:y:str))) = case (getNum x, getNum y) of
+    (10, 10) -> IntStr n (Str (y:str))
+    (10, _) -> IntStr n (parseIntStr (IntStr 0 (Str (y:str))))
+    _ -> parseIntStr (IntStr (n*10 + (getNum x)) (Str (y:str)))
+parseIntStr s = s
 
 
 parseIntFromStr :: String -> IntStr
 parseIntFromStr str = parseIntStr (IntStr 0 (Str str))
 
 parseMessage :: String -> LogMessage
-parseMessage (c : _ : list) = case (c, (parseIntFromStr list))of
+parseMessage str@(c : _ : list) = case (c, (parseIntFromStr list))of
     ('I', (IntStr i (Str s))) -> LogMessage Info i s
     ('W', (IntStr i (Str s))) -> LogMessage Warning i s
     ('E', (IntStr i (IntStr i2 (Str s)))) -> LogMessage (Error i) i2 s
-    (_, (IntStr n (Str s))) -> Unknown s
+    _ -> Unknown str 
+parseMessage s = Unknown s
 
-main = parseMessage "E 2 562 help help" (trace main)
+main = parseMessage "E 2 562 help help"
