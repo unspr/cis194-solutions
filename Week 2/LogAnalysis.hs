188c49ab3b5ef46392b97b3e5969b1b4e3677230
todd <unspr@qq.com>
2020-09-13 23:32:11 +0800

    complete log parse

--- Week 2/LogAnalysis.hs
+++ Week 2/LogAnalysis.hs
@@ -40,4 +40,45 @@ parseMessage str@(c : _ : list) = case (c, (parseIntFromStr list))of
     _ -> Unknown str 
 parseMessage s = Unknown s
 
-main = parseMessage "E 2 562 help help"
+-- res = parseMessage "E 2 562 help help"
+
+parse :: String -> [LogMessage]
+parse s = parseArray . lines $ s
+
+parseArray :: [String] -> [LogMessage]
+parseArray [] = []
+parseArray (s:list) = parseMessage s : parseArray list
+
+-- res = testParse parse 10 "error.log"
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) tree = tree
+insert logMessage@(LogMessage _ time _) (Node ltree node@(LogMessage _ nodeTime _) rtree)
+    | time < nodeTime = (Node (insert logMessage ltree) node rtree)
+    | time > nodeTime = (Node ltree node (insert logMessage rtree))
+insert logMessage Leaf = (Node Leaf logMessage Leaf) 
+insert _ _ = Leaf
+
+build :: [LogMessage] -> MessageTree
+build [] = Leaf
+build (h:list) = insert h (build list)
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node ltree node rtree) = inOrder ltree ++ [node] ++ inOrder rtree
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong list = logToStr . inOrder . build . importantLog $ list
+
+importantLog :: [LogMessage] -> [LogMessage]
+importantLog [] = []
+importantLog (h@(LogMessage (Error n) _ _):list)
+    | n >= 50 = h: importantLog list
+importantLog (_:list) = importantLog list     
+
+logToStr :: [LogMessage] -> [String]
+logToStr [] = []
+logToStr ((LogMessage (Error _) _ s):list) = s : logToStr list
+logToStr _ = []
+
+res = testWhatWentWrong parse whatWentWrong "sample.log"
