3d0d8a2b0320683e63c658d40a4b3613291cbeb5
todd <unspr@qq.com>
2020-09-21 23:05:57 +0800

    complete exercise 1 and 2 in week 3

--- Week 3/Golf.hs
+++ Week 3/Golf.hs
@@ -1,3 +1,19 @@
 module Golf where
 skips :: [a] -> [[a]]
-skips s = 
+skips s = (map $ skipAs s) [1..length s]
+
+skipAs :: [a] -> Int -> [a]
+skipAs list n = let l = drop (n-1) list in
+    case l of
+        [] -> []
+        (x:remain) -> [x] ++ skipAs remain n
+
+-- res = skips "ABCD"
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima full@(x:y:z:list)
+    | x < y && z < y = [y] ++ localMaxima (drop 1 full)
+    | True = localMaxima (drop 1 full)
+localMaxima _ = []
+
+res = localMaxima [2,9,5,6,1] 
