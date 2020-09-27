69afd346cf9af5f9215feaa96317e44d99944d96
todd <unspr@qq.com>
2020-09-27 23:11:17 +0800

    complete week 3

--- Week 3/Golf.hs
+++ Week 3/Golf.hs
@@ -1,4 +1,6 @@
 module Golf where
+import Data.List
+
 skips :: [a] -> [[a]]
 skips s = (map $ skipAs s) [1..length s]
 
@@ -16,4 +18,19 @@ localMaxima full@(x:y:z:list)
     | True = localMaxima (drop 1 full)
 localMaxima _ = []
 
-res = localMaxima [2,9,5,6,1] 
+-- res = localMaxima [2,9,5,6,1] 
+
+histogram :: [Integer] -> String
+histogram list = unlines $ reverse (["0123456789", "=========="] ++ dot list)
+
+dot :: [Integer] -> [String]
+dot [] = []
+dot list =  [listToXin (nub list)] ++ dot (list \\ (nub list))
+
+listToXin :: [Integer] -> String
+listToXin list = foldr func "          " list
+
+func :: Integer -> String -> String
+func n list = take (fromIntegral n) list ++ "*" ++ drop (fromIntegral (n + 1)) list
+
+res = putStr (histogram [1,4,5,4,6,6,3,4,2,4,9])
