9dfae69506ec00a0621b134ec8b419a749b63fe7
todd <unspr@qq.com>
2020-10-26 00:29:01 +0800

    e6 e7 and wee7 begin

--- Week 6/Fibonacci.hs
+++ Week 6/Fibonacci.hs
@@ -56,9 +56,30 @@ instance Num (Stream Integer) where
     fromInteger n = Stream n (streamRepeat 0)
     negate = streamMap (* (-1))
     (Stream a an) + (Stream b bn) = Stream (a+b) (an + bn)
-    (Stream a0 a') * b@(Stream b0 b') = Stream (a0*b0) (fromInteger a0*b' + a'*b)
+    -- using the followed expression in comments will lead to memory leak which caused maybe thunk bomb
+    -- (Stream a0 a') * b@(Stream b0 b') = Stream (a0*b0) (fromInteger a0*b' + a'*b)
+    (Stream a0 a') * b@(Stream b0 b') = Stream (a0*b0) (streamMap (* a0) b' + a'*b)
 
 instance Fractional (Stream Integer) where
-    a@(Stream a0 a') / b@(Stream b0 b') =  Stream (a0 `div` b0) (fromInteger (1 `div` b0))*(a' - a/b *b')
+    -- a@(Stream a0 a') / b@(Stream b0 b') =  Stream (a0 `div` b0) (fromInteger (1 `div` b0))*(a' - a/b *b')
+    -- In fibs3, 1 `div` b0 must equal to 1
+    -- a@(Stream a0 a') / b@(Stream b0 b') =  Stream (a0 `div` b0) (a' - a/b *b')
+    a@(Stream a0 a') / b@(Stream b0 b') =  Stream (a0 `div` b0) (streamMap (`div` b0) (a' - a/b *b'))
 
 fibs3 :: Stream Integer
+fibs3 = x / (1 - x - x^2)
+
+data Matrix = Matrix Integer Integer Integer Integer
+instance Num Matrix where
+    (Matrix a00 a01 a10 a11) * (Matrix b00 b01 b10 b11) = Matrix (a00*b00+a01*b10) (a00*b01+a01*b11) (a10*b00+a11*b10) (a10*b01+a11*b11)
+
+instance Show Matrix where
+    show (Matrix a00 a01 a10 a11)= "[ " ++ show a00 ++ " " ++ show a01 ++ "\n  " ++ show a10 ++ " " ++ show a11 ++ " ]\n" 
+
+fib4 :: Integer -> Integer
+fib4 0 = 0
+fib4 n = case (Matrix 1 1 1 0)^n of
+        (Matrix a _ _ _) -> a
+
+fibs4 :: [Integer]
+fibs4 = map fib4 [0..1000000]
