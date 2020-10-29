6ff5b7ad4f9203e673b641f2b4c416c76dadd5e7
todd <unspr@qq.com>
2020-10-29 08:28:41 +0800

    upload

--- Week 7/JoinList.hs
+++ Week 7/JoinList.hs
@@ -2,7 +2,7 @@ module JoinList where
 
 import Data.Monoid
 import Sized
-
+import Data.Maybe
 data JoinList m a = Empty
     | Single m a
     | Append m (JoinList m a) (JoinList m a)
@@ -17,4 +17,26 @@ tag (Append a _ _) = a
 tag Empty = mempty
 
 indexJ :: (Sized b, Monoid b) => Int -> JoinList b a -> Maybe a
-indexJ index jl = if size index == tag jl then jl else 
+indexJ idx Empty = Nothing
+indexJ idx (Single m e) = if Size idx == size m then Just e else Nothing
+indexJ idx (Append m l r) = let x = indexJ idx l in
+            case x of
+                (Just _) -> x
+                _ -> indexJ idx r
+
+dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+dropJ n Empty = Empty
+dropJ n jl
+    | n == 0 = jl
+    | n >= getSize (size (tag jl)) = Empty
+    
+dropJ n x@(Append m l r)
+    | n < getSize (size (tag l)) = dropJ n l
+    | n == getSize (size (tag l)) = r
+    | True = dropJ (n - (getSize (size (tag l)))) r
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ n Empty = Empty
+    | n < getSize (size (tag l)) = takeJ n l
+    | n == getSize (size (tag l)) = l
+    -- | True = takeJ (n - (getSize (size (tag l)))) r
