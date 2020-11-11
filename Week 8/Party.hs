20b54db5660e2b23bbfe3b7deaad0b3927aff640
todd <unspr@qq.com>
2020-11-11 08:26:41 +0800

    e2

--- Week 8/Party.hs
+++ Week 8/Party.hs
@@ -1,5 +1,7 @@
 module Party where
 import Employee
+import Data.Tree
+import Data.Monoid
 
 glCons :: Employee -> GuestList -> GuestList
 glCons e (GL list fun) = GL (e:list) (fun + empFun e)
@@ -12,3 +14,12 @@ moreFun :: GuestList -> GuestList -> GuestList
 moreFun xgl ygl = case compare xgl ygl of
                     GT -> xgl
                     _ -> ygl
+
+treeFold :: Monoid b => (a -> b -> b) -> b -> Tree a -> b
+treeFold f val t = foldr (\tree tmp -> treeFold f mempty tree <> tmp) 
+                         (f (rootLabel t) val)
+                         (subForest t)
+
+nextLevel :: Employee -> [(GuestList, GuestList)]
+-> (GuestList, GuestList)
+
