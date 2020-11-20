1a0365acc8eeb3421c2e913a75eddf322469dc41
todd <unspr@qq.com>
2020-11-20 08:28:43 +0800

    upload

--- Week 10/AParser.hs
+++ Week 10/AParser.hs
@@ -56,4 +56,10 @@ posInt = Parser f
 
 ------------------------------------------------------------
 -- Your code goes below here
+first :: (a -> b) -> (a,c) -> (b,c)
+first f (x, y) = (f x, y)
+
+instance Functor Parser where
+  fmap f (Parser (\x -> Maybe (y, String))) = Parser $ (\x -> )
+
 ------------------------------------------------------------
