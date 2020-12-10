a932e1d7c8fefd6d225c8252d3108d0d43a69ec3
todd <unspr@qq.com>
2020-12-11 00:14:28 +0800

    upload

--- Week 12/Risk.hs
+++ Week 12/Risk.hs
@@ -3,6 +3,8 @@
 module Risk where
 
 import Control.Monad.Random
+import Control.Monad.List
+import Data.List
 
 ------------------------------------------------------------
 -- Die values
@@ -26,3 +28,23 @@ die = getRandom
 type Army = Int
 
 data Battlefield = Battlefield { attackers :: Army, defenders :: Army }
+
+both :: (a -> b) -> (a, a) -> (b, b)
+both f (x, y) = (f x, f y)
+
+dice :: Army -> Rand StdGen [Army]
+dice n = replicateM n (getRandomR (1, 6)) >>= return.reverse.sort
+
+getUnit :: Battlefield -> (Army, Army)
+getUnit (Battlefield attack defend) = ((!!) (0:1:[1..]) attack, (!!) (0:1:repeat 2) defend)
+
+battle :: Battlefield -> Rand StdGen Battlefield
+battle b@(Battlefield attack defend) = do
+      let (x, y) = both dice $ getUnit b
+      ll <- x
+      rr <- y
+      let num = sum $ zipWith (\m n -> if m > n then 1 else 0) ll rr
+      Battlefield <$> return (attack - ((min (length ll) (length rr)) - num)) <*> return (defend - num)
+
+
+res = battle $ Battlefield 4 3
